457c131b023b03a48e563f29f169e69351d1018f
lightvsdarkness <v.a.pletnyakov@yandex.ru>
2015-04-10 09:47:12 +0300

    3

--- app/server.js
+++ app/server.js
@@ -1,12 +1,17 @@
-var http = require("http"),
-    server,
-    port = process.env.PORT || 1337;;
-
-server = http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World!\n");
+var express = require("express"),
+port = process.env.PORT || 1337,
+http = require("http"),
+app = express();
+// настроим статическую файловую папку
+// для маршрута по умолчанию
+// (см. также замечание о маршрутах далее)
+app.use(express.static(__dirname + "/client"));
+// создадим HTTP-сервер на базе Express
+http.createServer(app).listen(port);
+// настроим маршруты
+app.get("/hello", function (req, res) {
+res.send("Hello, World!");
 });
-
-server.listen(port);
-
-console.log("Server running on port 3000");
+app.get("/goodbye", function (req, res) {
+res.send("Goodbye, World!");
+});
