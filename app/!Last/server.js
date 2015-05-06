132fd3f9e0031ac2eccced8ead07d09d25fa306a
Emeri <pletnyakov.v.a.@yandex.ru>
2015-05-06 07:18:53 +0300

    dogtabs0

---
+++ app/!Last/server.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+port = process.env.PORT || 1337,
+http = require("http"),
+app = express();
+// настроим статическую файловую папку
+// для маршрута по умолчанию
+// (см. также замечание о маршрутах далее)
+app.use(express.static(__dirname + "/client"));
+// создадим HTTP-сервер на базе Express
+http.createServer(app).listen(port);
+//res.end("Hello, World!");
+// настроим маршруты
+app.get("/hello", function (req, res) {
+res.send("Hello, World!");
+});
+app.get("/goodbye", function (req, res) {
+res.send("Goodbye, World!");
+});
